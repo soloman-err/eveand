f836c05fab4336cef9c4031c3f499fa6dbdd4347
solo <soloman.err@.com>
2023-06-19 21:01:36 +0600

    firebase configuration implemented

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAtpAESgGZ6SNYHm4pQWqRBrzaDQA1BtA8',
+  authDomain: 'eve-and.firebaseapp.com',
+  projectId: 'eve-and',
+  storageBucket: 'eve-and.appspot.com',
+  messagingSenderId: '1003444386383',
+  appId: '1:1003444386383:web:5a4a0a6145ae15521d083a',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
