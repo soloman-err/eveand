12292372bbf73bc0821b812495eb3e03e8c6cfa9
solo <soloman.err@.com>
2023-06-19 19:45:07 +0600

    Popular event updated to implementing resposiveness

--- src/pages/home/popularEvents/PopularEvents.jsx
+++ src/pages/home/popularEvents/PopularEvents.jsx
@@ -3,9 +3,9 @@ import EventCard from './EventCard';
 
 const PopularEvents = () => {
   return (
-    <section className="mt-20 mx-20">
+    <section className="mt-20 mx-5 lg:mx-20">
       <h1 className="font-bold text-4xl border-l-8 border-blue-500 pl-2 my-5">Popular Events</h1>
-      <div className="grid grid-cols-3 gap-3 mt-">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
         <EventCard />
         <EventCard />
         <EventCard />
